819d82d3a46834a54d29e01e780406bd4028f066
dc1308 <75678223+dc1308@users.noreply.github.com>
2021-04-02 18:58:48 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,29 @@
+class Ball {
+    constructor(x, y) {
+      var options = {
+        'density':1.5,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.circle(x, y, 50 , options);
+      this.r = 30;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('blue')
+      fill('red')
+      rectMode(CENTER)
+      ellipse(0, 0, this.r, this.r);
+      pop();
+    };
+  }
+  
